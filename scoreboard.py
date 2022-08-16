7c6cd8323b7fd147e9e15ce8cb89c4e56eabf012
akhiller30 <86208846+akhiller30@users.noreply.github.com>
2022-08-16 15:57:27 -0700

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -8,18 +8,32 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt", mode="r") as file:
+            self.highscore = int(file.read())
         self.color("yellow")
         self.pu()
         self.goto(0, 270)
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
         self.hideturtle()
+        self.update_scoreboard()
     
     def inc_score(self):
         self.score += 1
-        self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.update_scoreboard()
     
-    def game_over(self):
-        self.color("red")
-        self.goto(0,0)
-        self.write(f"GAME OVER!", align=ALIGNMENT,font=("Courier", 36, "normal"))
+    def reset(self): 
+        if self.score > self.highscore:
+            self.highscore = self.score
+        self.score = 0
+        with open("data.txt", mode="w") as file:
+            file.write(f"{self.highscore}")
+        self.update_scoreboard()
+    
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.highscore}", align=ALIGNMENT, font=FONT)
+   
+   # def game_over(self):
+     #   self.color("red")
+     #   self.goto(0,0)
+     #   self.write(f"GAME OVER!", align=ALIGNMENT,font=("Courier", 36, "normal"))
+
